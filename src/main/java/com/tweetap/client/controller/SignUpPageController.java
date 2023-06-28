3dda70d91e6080d96774eeff19b4e42e53af02e5
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-29 01:45:09 +0330

    signuppage.fxml set controller

---
+++ src/main/java/com/tweetap/client/controller/SignUpPageController.java
@@ -0,0 +1,89 @@
+package com.tweetap.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+public class SignUpPageController
+{
+    @FXML
+    public TextField signUpUserNameTextField;
+    @FXML
+    public TextField signUpNameTextField;
+    @FXML
+    public TextField signUpFamilyTextField;
+    @FXML
+    public TextField signUpEmailTextField;
+    @FXML
+    public TextField signUpPhoneTextField;
+    @FXML
+    public ComboBox signUpCountryComboText;
+    @FXML
+    public DatePicker signUpBirthDayDatePicker;
+    @FXML
+    public PasswordField signUpPasswordField;
+    @FXML
+    public PasswordField signUpConfirmPasswordField;
+    @FXML
+    public Button signUpSignUpButton;
+
+    @FXML
+    public void signUpUserNameTextFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpNameTextFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    public void signUpFamilyTextFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpEmailTextFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpPhoneTextFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpCountryComboTextOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpBirthDatePickerOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpPasswordFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpConfirmPasswordFieldOnKeyPressed(KeyEvent keyEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+
+    @FXML
+    public void signUpSignUpButtonOnAction(ActionEvent actionEvent)
+    {
+        // TODO : handle when user pressed 'Enter' switch to next field and save this text to suitable variable
+    }
+}
