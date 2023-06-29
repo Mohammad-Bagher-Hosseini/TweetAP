05959f1d33fdfb5638f8ba046e70ed8c08f9d9df
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-29 21:48:44 +0330

    quoteview.fxml+ set controller

--- src/main/java/com/tweetap/client/controller/graphic/QuoteViewController.java
+++ src/main/java/com/tweetap/client/controller/graphic/QuoteViewController.java
@@ -2,12 +2,18 @@ package com.tweetap.client.controller.graphic;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class QuoteViewController
 {
     @FXML
     public Label quoteViewUserNameLabel;
+    @FXML
+    public TextArea quoteViewTextArea;
+    @FXML
+    public ImageView quoteViewImageView;
 
     @FXML
     public void quoteViewUserNameLabelOnMouseClicked(MouseEvent mouseEvent)
