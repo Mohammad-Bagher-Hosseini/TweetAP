fbeb4b2c81da6384a89656bf5a0cad42b34d35c5
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-29 21:47:58 +0330

    quoteview.fxml set controller

---
+++ src/main/java/com/tweetap/client/controller/graphic/QuoteViewController.java
@@ -0,0 +1,17 @@
+package com.tweetap.client.controller.graphic;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class QuoteViewController
+{
+    @FXML
+    public Label quoteViewUserNameLabel;
+
+    @FXML
+    public void quoteViewUserNameLabelOnMouseClicked(MouseEvent mouseEvent)
+    {
+        // TODO : show selected user's profile
+    }
+}
