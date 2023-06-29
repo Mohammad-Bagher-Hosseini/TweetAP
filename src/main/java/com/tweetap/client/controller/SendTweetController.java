adea663022f2c5834974387656531a0a320385ee
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-29 19:44:10 +0330

    sendtweet.fxml set controller

---
+++ src/main/java/com/tweetap/client/controller/SendTweetController.java
@@ -0,0 +1,72 @@
+package com.tweetap.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class SendTweetController
+{
+
+    @FXML
+    public Button sendTweetHomeButton;
+    @FXML
+    public Button sendTweetSearchButton;
+    @FXML
+    public Button sendTweetProfileButton;
+    @FXML
+    public Button sendTweetExitButton;
+    @FXML
+    public Label sendTweetUserNameLabel;
+    @FXML
+    public TextArea sendTweetInputTextArea;
+    @FXML
+    public ImageView sendTweetInputImageView;
+    @FXML
+    public TextField sendTweetImagePathTextFiled;
+    @FXML
+    public Button sendTweetSearchImagePathButton;
+    @FXML
+    public ImageView sendTweetAvatarImageView;
+    @FXML
+    public Button sendTweetSendTweetButton;
+
+    @FXML
+    public void sendTweetHomeButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : switch to suitable scene
+    }
+
+    @FXML
+    public void sendTweetSearchButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : switch to suitable scene
+    }
+
+    @FXML
+    public void sendTweetProfileButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : switch to suitable scene
+    }
+
+    @FXML
+    public void sendTweetExitButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : Exit
+    }
+
+    @FXML
+    public void sendTweetSearchImagePathButtonOnAction(ActionEvent actionEvent)
+    {
+        // TODO : process input variables
+    }
+
+    @FXML
+    public void sendTweetSendTweetButtonOnAction(ActionEvent actionEvent)
+    {
+        // TODO : process input variables
+    }
+}
