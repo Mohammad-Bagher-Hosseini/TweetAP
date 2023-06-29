cb051f150cf4fcb80929113dbc473e5721de39b9
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-29 21:41:54 +0330

    retweetview.fxml.fxml set controller

---
+++ src/main/java/com/tweetap/client/controller/graphic/RetweetViewController.java
@@ -0,0 +1,17 @@
+package com.tweetap.client.controller.graphic;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class RetweetViewController
+{
+    @FXML
+    public Label retweetViewUserNameLabel;
+
+    @FXML
+    public void retweetViewUserNameLabelOnMouseClicked(MouseEvent mouseEvent)
+    {
+        //TODO : show selected user's profile
+    }
+}
