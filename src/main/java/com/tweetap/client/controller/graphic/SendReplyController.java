14533f53f9332889e104a39deb2375d2e6423a23
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-30 04:32:27 +0330

    sendreply_pop_up.fxml set controller

---
+++ src/main/java/com/tweetap/client/controller/graphic/SendReplyController.java
@@ -0,0 +1,20 @@
+package com.tweetap.client.controller.graphic;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class SendReplyController
+{
+    @FXML
+    public TextArea sendReplyTextArea;
+    @FXML
+    public Button sendReplySendReplyButton;
+
+    @FXML
+    public void sendReplySendReplyButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : send reply and close the pop_up
+    }
+}
