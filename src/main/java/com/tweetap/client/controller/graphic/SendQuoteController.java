7067dedaff1e44bec129723a25c3b280cf372988
M.B.Hosseini <mohammad.h.b.10000@gmail.com>
2023-06-30 04:26:30 +0330

    sendquote_pop_up.fxml set controller

---
+++ src/main/java/com/tweetap/client/controller/graphic/SendQuoteController.java
@@ -0,0 +1,34 @@
+package com.tweetap.client.controller.graphic;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class SendQuoteController
+{
+    @FXML
+    public TextField sendQuoteImagePathTextFiled;
+    @FXML
+    public Button sendQuoteSearchImagePathButton;
+    @FXML
+    public ImageView sendQuoteImageView;
+    @FXML
+    public Button sendQuoteSendQuoteButton;
+    @FXML
+    public TextArea sendQuoteTextArea;
+
+    @FXML
+    public void sendQuoteSearchImagePathButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : set image
+    }
+
+    @FXML
+    public void sendQuoteSendQuoteButtonOnAction(ActionEvent actionEvent)
+    {
+        //TODO : send quote and close the pop_up
+    }
+}
