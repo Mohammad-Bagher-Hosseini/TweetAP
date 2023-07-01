c7563bcf3bc48f138cd5d3c565e1d143b2dccd60
Mohammad-Bagher-Hosseini <124880487+Mohammad-Bagher-Hosseini@users.noreply.github.com>
2023-07-01 23:11:07 +0330

    add image process to MiniProfileController.java

--- src/main/java/com/tweetap/client/controller/graphic/MiniProfileController.java
+++ src/main/java/com/tweetap/client/controller/graphic/MiniProfileController.java
@@ -3,13 +3,22 @@ package com.tweetap.client.controller.graphic;
 import com.tweetap.entities.user.MiniUser;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MiniProfileController implements HasStage
 {
     @FXML
     public Label userNameLabel;
+    @FXML
+    public ImageView avatarImageView;
     private Stage stage;
 
     @FXML
@@ -21,6 +30,15 @@ public class MiniProfileController implements HasStage
     public void showMiniUser(MiniUser miniUser)
     {
         userNameLabel.setText(miniUser.getName() + " " + miniUser.getFamily() + "@" + miniUser.getUserName());
+        ByteArrayOutputStream byteImage = new ByteArrayOutputStream();
+        try
+        {
+            ImageIO.write(miniUser.getAvatar().getImage(), "jpg", byteImage);
+        } catch (IOException e)
+        {
+            System.out.println("MiniProfile -> showMiniUser -> writeImage in byteArray error : can't work with userAvatar!");
+        }
+        avatarImageView.setImage(new Image(new ByteArrayInputStream(byteImage.toByteArray())));
         // TODO: avatar
     }
 
