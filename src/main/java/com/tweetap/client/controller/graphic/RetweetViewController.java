49826263c4031b41bd51d722b80adac04a6a27fa
Mohammad-Bagher-Hosseini <124880487+Mohammad-Bagher-Hosseini@users.noreply.github.com>
2023-07-01 16:13:36 +0330

    retweetview.fxml reform

--- src/main/java/com/tweetap/client/controller/graphic/RetweetViewController.java
+++ src/main/java/com/tweetap/client/controller/graphic/RetweetViewController.java
@@ -4,11 +4,14 @@ import com.tweetap.entities.tweet.Retweet;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class RetweetViewController
 {
     @FXML
     public Label retweetViewUserNameLabel;
+    @FXML
+    public VBox tweetVbox;
 
     @FXML
     public void retweetViewUserNameLabelOnMouseClicked(MouseEvent mouseEvent)
