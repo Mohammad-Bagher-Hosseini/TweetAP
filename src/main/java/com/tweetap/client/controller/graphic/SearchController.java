6321d1b2618f14c09dbac7b6a365423a1ae58580
Parsa Salamatipour <sa.parsa20@gmail.com>
2023-07-02 01:22:54 +0330

    SearchController done

--- src/main/java/com/tweetap/client/controller/graphic/SearchController.java
+++ src/main/java/com/tweetap/client/controller/graphic/SearchController.java
@@ -1,7 +1,13 @@
 package com.tweetap.client.controller.graphic;
 
+import com.tweetap.MainClient;
+import com.tweetap.client.controller.ControllerCommands;
+import com.tweetap.entities.exception.TwitException;
+import com.tweetap.entities.exception.hashtag.HashtagException;
+import com.tweetap.entities.tweet.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
@@ -18,40 +24,80 @@ public class SearchController implements HasStage
     @FXML
     public Button exitButton;
     @FXML
-    public Button searchSearchSearchButton;
-    private Stage stage;
     public Button searchSearchButton;
+    private Stage stage;
     @FXML
     public VBox findUsersVBox;
 
     @FXML
     public void homeButtonOnAction(ActionEvent actionEvent)
     {
-        //TODO : switch to suitable scene
+        MainClient.loadPage(stage, "timeline.fxml");
     }
 
     @FXML
     public void profileButtonOnAction(ActionEvent actionEvent)
     {
-        //TODO : switch to suitable scene
+        MainClient.loadPage(stage, "profile.fxml");
     }
 
     @FXML
     public void tweetButtonOnAction(ActionEvent actionEvent)
     {
-        //TODO : switch to suitable scene
+        MainClient.loadPage(stage, "sendtweet.fxml");
     }
 
     @FXML
     public void exitButtonOnAction(ActionEvent actionEvent)
     {
-        //TODO : Exit
+        System.exit(0);
     }
 
     @FXML
-    public void searchSearchButtonOnAction(ActionEvent actionEvent)
+    public void searchButtonOnAction(ActionEvent actionEvent)
     {
-        // TODO : process input variables and show results
+        String hashtags = "#hello"; // TODO
+        try
+        {
+            TimeLine timeLine = ControllerCommands.searchForHashtag(hashtags);
+            for(BaseTweet baseTweet : timeLine)
+            {
+                if(baseTweet instanceof Tweet tweet)
+                    addTweet(tweet);
+                else if(baseTweet instanceof Quote quote)
+                    addQuote(quote);
+                else if (baseTweet instanceof Retweet retweet)
+                    addRetweet(retweet);
+            }
+        } catch (HashtagException e)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Wrong hashtag format");
+            alert.show();
+        } catch (TwitException e)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Something went wrong while showing the hashtags");
+            alert.show();
+        }
+    }
+
+    private void addTweet(Tweet tweet)
+    {
+        TweetViewController tweetViewController = MainClient.loadPage(findUsersVBox, stage, "tweetview.fxml");
+        tweetViewController.showTweet(tweet);
+    }
+
+    private void addRetweet(Retweet retweet)
+    {
+        RetweetViewController retweetViewController = MainClient.loadPage(findUsersVBox, stage, "retweetview.fxml");
+        retweetViewController.showRetweet(retweet);
+    }
+
+    private void addQuote(Quote quote)
+    {
+        QuoteViewController quoteViewController = MainClient.loadPage(findUsersVBox, stage, "quoteview.fxml");
+        quoteViewController.showQuote(quote);
     }
 
     public void setStage(Stage stage)
